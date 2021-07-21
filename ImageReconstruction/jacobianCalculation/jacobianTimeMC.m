2b3d93b76e6c0f64da087db139fe810a9a27487f
jiang <jing.jing.jiang@hotmail.com>
2021-07-21 16:15:27 +0200

    add classes

---
+++ ImageReconstruction/jacobianCalculation/jacobianTimeMC.m
@@ -0,0 +1,14 @@
+%>@brief Brief description of the function
+%>
+%> calculate jacobian for time domain modality 
+%> based on monte carlo simulation
+%>
+%>@param volIn tissue volume
+%>@param cfg config struct including parameters for the forward models
+%>           
+%>
+%> @retval jac jacobian
+%> @retval out2 return value for the second output variable
+ 
+function [jac, varargout] = jacobianTimeMC(volIn, cfg, varargin)
+end
---
+++ ImageReconstruction/reconstructionTime.m
@@ -0,0 +1,13 @@
+%>@brief Brief description of the function
+%>
+%> image reconstruction for time domain modality 
+%>
+%>@param volIn tissue volume
+%>@param cfg config struct including parameters for the forward models
+%>           
+%>
+%> @retval volOut reconstructed volume
+%> @retval out2 return value for the second output variable
+ 
+function [volOut, varargout] = reconstructionTime(volIn, cfg, varargin)
+end
