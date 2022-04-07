ec563168712b3cfb7078b73ae675f3c8ae442572
Emanuele KK <emanuele.r90@gmail.com>
2022-04-07 07:26:32 +0200

    reverted back for code clarity

--- Piccolo_data_correction/timing_data_correction_z3_par.m
+++ Piccolo_data_correction/timing_data_correction_z3_par.m
@@ -30,16 +30,16 @@ proj_name = 'ICGlipid_20220311';
 name_fld_corrected = 'timing_data_corrected';
 Prefix = 'laser';
 
-waveList = [ 780 830 ];   % Example [689 725 802];
+waveList = [ 725 838 ];   % Example [689 725 802];
 
-sources =  [1:11];
+sources =  [7];
 
 path_prj = fullfile(Path_base, proj_name);
 hdf5_file = fullfile(path_prj, strcat(Prefix,'-', proj_name,'.hdf5'));
 % hdf5_file = [Path_base '/' proj_name '/laser-20220228_PETG_homo.hdf5'];
 info = h5info(hdf5_file, '/Data');
 
-if exist(fullfile(path_prj, name_fld_corrected))==0
+if exist(fullfile(path_prj, name_fld_corrected), 'dir')==0
     mkdir(fullfile(path_prj, name_fld_corrected))
 else
     rmdir(fullfile(path_prj, name_fld_corrected), 's') %remove it first if already exist
@@ -118,15 +118,7 @@ parfor i_id = 1:length(ids)
     %timing_response_1 = timing_response;
     %timing_data_tmp = zeros(size(timing_response)); % added by jingjing 2021.05.11
     
-    %Generate static variables once
-    shared_data = struct;
-    shared_data.sig_interp = 100;
-    shared_data.t1 = 0:(length(timing_response)-1);
-    shared_data.t3 = (0:(length(timing_response)*shared_data.sig_interp)-1)/shared_data.sig_interp;
-    
     for col_i = 1:cols
-                
-        shared_data.l_end = (256*shared_data.sig_interp)/col_cal(col_i);
 
         for row_i = 1:rows
             
@@ -134,7 +126,7 @@ parfor i_id = 1:length(ids)
                    ((row_i==17&&col_i==24)) || (col_i == 32) && (row_i == 2)  % changed by jingjing 2021.10.11
                 timing_response(:, row_i, col_i) = 0; % added by jingjing 2021.05.11
             else
-                timing_response(:, row_i, col_i) = correct_histogram_par(timing_response(:, row_i, col_i), add_corr(row_i, col_i), col_cal(col_i), shared_data);
+                timing_response(:, row_i, col_i) = correct_histogram(timing_response(:, row_i, col_i), add_corr(row_i, col_i), col_cal(col_i));
              
             end
             
@@ -155,76 +147,4 @@ fprintf('Time elapsed for computation: %f sec\n',toc);
 
 function save_correct_hist(file_name, timing_response)
         save(file_name, 'timing_response');
-end
-
-
-function [histogram] = correct_histogram_par(x, add_corr, col_cal, data_struct)
-%%%Modified version of correct_histogram.m that avoid some redudant computation
-%%%%% Load signal and correct for dnl, added 12.11.18 SL
-sig_interp = data_struct.sig_interp;
-
-x = x - median(x);  %% Subtracts any dark counts
-
-x_counts = sum(x);
-
-%%% Need to apply any correction of DNL before
-%%% interpolation etc!! %%%
-
-x = flipud(x);
-% LM: interp1 seems much faster than interp, though I am not sure about the
-% accuracy tradeoff
-
-y = interp1(data_struct.t1, double(x), data_struct.t3, 'spline');   %% interpolate x at rate sig_interp
-
-%l_start = length(y);        %% length before scaling
-
-%l_end = length(y)*col_cal(col_i);   %% length after scaling
-
-%%%% This part of the code just makes y the correct length,
-%%%% this needs to account for the slicing of the TPSF
-
-l_end = data_struct.l_end;
-
-if l_end < length(y)
-    sig_diff = round(length(y) - l_end);
-    y = y(1:(end - sig_diff));
-    
-elseif l_end > length(y)
-    sig_diff = round(l_end - length(y));
-    y = [y(1:end); zeros(sig_diff,1)];
-    
-end
-%%%%% Add shift for additive part of the chip calibration
-
-bins_shift = (add_corr*sig_interp)/col_cal;
-bins_shift = round(bins_shift);
-
-%try
-y = circshift(y,-bins_shift);
-%catch
-%    temp = 1;
-%end
-
-%%%%% Interpolate to 256 bins to correct for LSB
-
-x1 = 1:(length(y));
-x2_start = 1; % starts at 1 because of the flip.
-x2_end = (length(y)-(length(y)/256));
-x2 = x2_start:((x2_end-x2_start)/255):x2_end;
-y1 = interp1(x1, y, x2); % spline is unneccessay when downsampling LM
-y1 = round(y1)'; % transpose for convenience
-TXX = y1(:) < 0;
-y1(TXX) = 0;
-
-%%%%% Finally correct number of photons to initial value so
-%%%%% that intensity is correct
-
-y_counts = sum(y1); %% Total counts after correction
-
-for i_y = 1:length(y1)
-    y1(i_y) = (y1(i_y)/y_counts)*x_counts;
-end
-
-%%%%% And load back into timing response
-histogram = flipud(y1);
 end
